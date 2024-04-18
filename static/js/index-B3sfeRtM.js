0a4214eea35f7b861acd01b023c0dc7346889a3d
memooai <167413279+memooai@users.noreply.github.com>
2024-04-18 11:42:28 +0000

    Deploying to gh-pages from @ memoo-ai/memoo-ai.github.io@df378f5b2b640319d79c23b06f3b74ac9551562d 🚀

---
+++ static/js/index-B3sfeRtM.js
@@ -0,0 +1,1 @@
+import{j as e}from"./rainbowkit-D4A4C0pO.js";import"./react-BQSzCOjX.js";const s=()=>e.jsx("div",{className:"mx-[auto] max-w-[94.5rem] pb-[3rem] mt-8",children:e.jsx("div",{className:"md:block hidden ",children:"Not found"})});export{s as default};
