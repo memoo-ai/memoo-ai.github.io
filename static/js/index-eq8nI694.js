7ff6019b7154a56cfe8cd533656ea40d34a923e2
mickwif <92444797+mickwif@users.noreply.github.com>
2024-04-20 06:52:36 +0000

    Deploying to gh-pages from @ memoo-ai/memoo-ai.github.io@d5fa610c3e45ff5f1f4d9674fe312227003c9954 🚀

--- static/js/index-eq8nI694.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./rainbowkit-C6AzvjOR.js";import"./react-BQSzCOjX.js";const s=()=>e.jsx("div",{className:"mx-[auto] max-w-[94.5rem] pb-[3rem] mt-8",children:e.jsx("div",{className:"md:block hidden ",children:"Not found"})});export{s as default};
