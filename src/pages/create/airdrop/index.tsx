28f3c7d092dae469d2e8c195dd2a7c57cca2a84b
Zone <myz.defi@gmail.com>
2024-05-27 16:37:26 +0800

    wip(airdrop): progress

--- src/pages/create/airdrop/index.tsx
+++ src/pages/create/airdrop/index.tsx
@@ -8,6 +8,7 @@ import PublicSale from '../common/public-sale';
 import IDODetail from '../common/ido-detail';
 import Banner from '../common/banner';
 import Profile from '../common/profile';
+import Progress from '../common/progress';
 
 interface AirdropContext {
   stage: TokenCreateStage;
@@ -16,24 +17,29 @@ interface AirdropContext {
 export const AirdropContext = createContext<AirdropContext>({ stage: 'in-queue' });
 
 export default function Airdrop() {
-  const [stage, setStage] = useState<TokenCreateStage>('launch');
+  const [stage, setStage] = useState<TokenCreateStage>('in-queue');
 
   const context: AirdropContext = useMemo(() => ({ stage }), [stage]);
 
   return (
-    <div className="airdrop pb-16">
-      <div className="airdrop_left flex flex-col gap-y-3.5">
-        <AirdropContext.Provider value={context}>
-          <Status />
-          {stage === 'launch' && <PublicSale />}
-          {stage === 'imo' && <IMOParticipate />}
-          <AirdropClaim />
-          <IDODetail />
-        </AirdropContext.Provider>
-      </div>
-      <div className="airdrop_right flex flex-col">
-        <Banner />
-        <Profile />
+    <div className="pt-[70px] flex flex-col items-center">
+      <AirdropContext.Provider value={context}>
+        <Progress />
+      </AirdropContext.Provider>
+      <div className="airdrop pb-16">
+        <div className="airdrop_left flex flex-col gap-y-3.5">
+          <AirdropContext.Provider value={context}>
+            <Status />
+            {stage === 'launch' && <PublicSale />}
+            {stage === 'imo' && <IMOParticipate />}
+            <AirdropClaim />
+            <IDODetail />
+          </AirdropContext.Provider>
+        </div>
+        <div className="airdrop_right flex flex-col">
+          <Banner />
+          <Profile />
+        </div>
       </div>
     </div>
   );
