e93173c4db3ea05d387266771be3a32e9b26c15c
Zone <myz.defi@gmail.com>
2024-05-30 10:48:41 +0800

    chore: add VITE_DEMO_TOKEN

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -6,9 +6,7 @@ const http = axios.create({
 });
 
 http.interceptors.request.use((config) => {
-  config.headers[
-    'Authorization'
-  ] = `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyZXNzIjoiMHgyNDMwNzM2MzlCNzE4OTk3N0U1RGJBN2VkRTFiNWY1RDE4NTMxNDE4IiwiY2hhaW4iOiIiLCJleHAiOjE3MTY5OTMyNTksIm9yaWdfaWF0IjoxNzE2OTA2ODU5fQ.v1w549bCDrcu2l-79YkKIJSbwvEUiseo7Oi6YbE9xk9pW8UNEwTuFJc9Ri7v8Fz2JfO8kAXfMGCGb39AX6one8aGJN4F7HKJ_zCC030JJ9VJ217ZVlHLleyAcM4hH5kMfSlTaeOJkOQvWleMGRjncJx0-sBCQk0hWVKBBFfDT9A9PP7XuCSXmtNQXIinrlW8D9uF88qfCbE0eYsvT9YMvNluN5fwNTEPMsSeCV84bfMuYh1Lzse-LjMTVRwBqR90tHCfGOa65OeG5rshJhEATK10CPZpDG_Sl_DNaKb03yP-hWNL4ImuQNdG0KyUqZIn83yPHh_dTH_uUqckhrOvlw`;
+  config.headers['Authorization'] = `Bearer ${import.meta.env.VITE_DEMO_TOKEN}`;
   return config;
 });
 
