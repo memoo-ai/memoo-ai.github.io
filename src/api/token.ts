6e386eaab08b46600e5c1f20735469b68c7616a0
mickywif <mickywif@gmail.com>
2024-06-11 10:52:40 +0800

    fix tsc error

--- src/api/token.ts
+++ src/api/token.ts
@@ -21,25 +21,24 @@ export interface ITokenSaveData {
   banners: string[];
 }
 
-const getFormData = (data: ITokenSaveData) => {
-  const formData = new FormData();
-  if (data.icon && data.icon instanceof File) {
-    formData.append('icon', data.icon);
-  }
-  if (data.banners && data.banners instanceof File) {
-    formData.append('banners', data.banners);
-  }
-  formData.append('preLaunchDuration', data.preLaunchDuration);
-  formData.append('preMarketAcquisition', data.preMarketAcquisition);
-  formData.append('projectDescription', data.projectDescription || '');
-  formData.append('telegram', data.telegram || '');
-  formData.append('ticker', data.ticker);
-  formData.append('tokenName', data.tokenName);
-  return formData;
-};
+// const getFormData = (data: ITokenSaveData) => {
+//   const formData = new FormData();
+//   if (data.icon && data.icon instanceof File) {
+//     formData.append('icon', data.icon);
+//   }
+//   if (data.banners && data.banners instanceof File) {
+//     formData.append('banners', data.banners);
+//   }
+//   formData.append('preLaunchDuration', data.preLaunchDuration);
+//   formData.append('preMarketAcquisition', data.preMarketAcquisition);
+//   formData.append('projectDescription', data.projectDescription || '');
+//   formData.append('telegram', data.telegram || '');
+//   formData.append('ticker', data.ticker);
+//   formData.append('tokenName', data.tokenName);
+//   return formData;
+// };
 
 export const saveTokenCraft = (data: ITokenSaveData) => {
-  const formData = getFormData(data);
   return http.post(`${prefix}/web-oriented/token`, data);
 };
 
