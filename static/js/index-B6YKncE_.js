b8ecb028f208d70818ba69d1605a8beeca641495
SnYang096 <168965522+SnYang096@users.noreply.github.com>
2024-06-12 07:25:00 +0000

    Deploying to gh-pages from @ memoo-ai/memoo-ai.github.io@7881e2e31313ee8df141a2d3d71520a6c0bdcd91 🚀

---
+++ static/js/index-B6YKncE_.js
@@ -0,0 +1,1 @@
+import{j as e}from"./rainbowkit-FC5XYMsa.js";import"./react-Dusg2Zfy.js";const s=()=>e.jsx("div",{className:"mx-[auto] max-w-[94.5rem] pb-[3rem] mt-8",children:e.jsx("div",{className:"md:block hidden ",children:"Not found"})});export{s as default};
