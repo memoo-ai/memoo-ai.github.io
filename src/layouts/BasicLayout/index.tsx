13a6742e9162fa403838a4079e28806d71ee72d2
Zone <myz.defi@gmail.com>
2024-06-13 15:54:49 +0800

    fix: no rawSignature

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import styles from './index.module.scss';
 import Header from '@/components/Header';
@@ -6,7 +6,29 @@ import Toast from '@/components/Toast';
 import Footer from '@/components/Footer';
 import MobilePage from './mobile';
 import isMobile from 'is-mobile';
+import { useEthersSigner } from '@/hooks/useEthers';
+import { useAccountEffect } from 'wagmi';
 const BasicLayout: React.FC = () => {
+  const signer = useEthersSigner({ chainId: Number(import.meta.env.VITE_NODE_CHAIN_ID) });
+  const [connected, setConnected] = useState(false);
+
+  useAccountEffect({
+    onConnect(data) {
+      setConnected(true);
+    },
+  });
+
+  useEffect(() => {
+    if (connected && signer) {
+      (async () => {
+        const msg = String(Date.now());
+        const rawSignature = await signer.signMessage(msg);
+        console.log('rawSignature', rawSignature);
+        // TODO
+      })();
+    }
+  }, [connected, signer]);
+
   return (
     <div>
       {isMobile() && <MobilePage />}
