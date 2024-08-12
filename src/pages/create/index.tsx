91172e3b343c27ce94a0c49e1f5c908f91de09e3
SnYang096 <SnYang096@proton.me>
2024-08-12 15:36:29 +0800

    fix: stage by launch

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -63,7 +63,7 @@ const PreLaunchDurationOptions = [
   { label: '3 days', value: PreLaunchDurationEnum['3DAYS'] },
 ];
 const CurrentProductDescriptions = [
-  'Tokenomics for Meme Token',
+  'Tokenomics for Meme Tokens',
   'Standardize Smart Contract Rails',
   'Simplified IMO & LP Management',
   'Fair Launch Policy',
