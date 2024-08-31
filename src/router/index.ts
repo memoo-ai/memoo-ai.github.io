bb2ddb46afa2db9895c691cf7fbed414a2ac4463
mickywif <mickywif@gmail.com>
2024-08-31 10:42:42 +0800

    feat: only show homepage, hide other routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,32 +7,40 @@ const routeConfig: RouteObject[] = [
     Component: lazy(() => import('../layouts/BasicLayout')),
     children: [
       {
-        path: '/home',
+        path: '/',
         Component: lazy(() => import('../pages/home')),
       },
       {
-        path: '/gecko',
-        Component: lazy(() => import('../pages/gecko')),
+        path: '/home',
+        Component: lazy(() => import('../pages/home')),
       },
+      // {
+      //   path: '/gecko',
+      //   Component: lazy(() => import('../pages/gecko')),
+      // },
+      // {
+      //   path: '/',
+      //   Component: lazy(() => import('../pages/launchpad')),
+      // },
+      // {
+      //   path: '/dashboard',
+      //   Component: lazy(() => import('../pages/dashboard')),
+      // },
+      // {
+      //   path: '/create_token',
+      //   Component: lazy(() => import('../pages/create')),
+      // },
+      // {
+      //   path: '/airdrop/:ticker',
+      //   Component: lazy(() => import('../pages/airdrop')),
+      // },
+      // {
+      //   path: '/share',
+      //   Component: lazy(() => import('../pages/share')),
+      // },
       {
-        path: '/',
-        Component: lazy(() => import('../pages/launchpad')),
-      },
-      {
-        path: '/dashboard',
-        Component: lazy(() => import('../pages/dashboard')),
-      },
-      {
-        path: '/create_token',
-        Component: lazy(() => import('../pages/create')),
-      },
-      {
-        path: '/airdrop/:ticker',
-        Component: lazy(() => import('../pages/airdrop')),
-      },
-      {
-        path: '/share',
-        Component: lazy(() => import('../pages/share')),
+        path: '*',
+        Component: lazy(() => import('../pages/not-found')),
       },
     ],
   },
