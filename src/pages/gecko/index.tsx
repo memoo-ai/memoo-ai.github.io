c5702aa5d2bd9da67bf35ef6bb155badcac1d752
SnYang096 <SnYang096@proton.me>
2024-10-18 15:45:00 +0800

    fix: payment unsuccessful

--- src/pages/gecko/index.tsx
+++ src/pages/gecko/index.tsx
@@ -52,43 +52,43 @@ const Gecko = () => {
       const { data } = tab === 'trending' ? await getTrendingTokens(params) : await getTopTokens(params);
       // console.log(data);
       if (data) {
-        // setData(data.records ?? []);
-        setData([
-          {
-            icon: 'string',
-            increase1H: 1,
-            increase24H: 1,
-            marketCap: 1,
-            memooScore: 100,
-            price: 1,
-            ticker: '1',
-            tokenName: 'string',
-            volume24H: 1,
-            totalScore: 1,
-            created: '196d',
-            liquidity: '6.5m',
-            holders: '114k',
+        setData(data.records ?? []);
+        // setData([
+        //   {
+        //     icon: 'string',
+        //     increase1H: 1,
+        //     increase24H: 1,
+        //     marketCap: 1,
+        //     memooScore: 100,
+        //     price: 1,
+        //     ticker: '1',
+        //     tokenName: 'string',
+        //     volume24H: 1,
+        //     totalScore: 1,
+        //     created: '196d',
+        //     liquidity: '6.5m',
+        //     holders: '114k',
 
-            vol1h: '235m',
-          },
-          {
-            icon: 'string',
-            increase1H: 1,
-            increase24H: 1,
-            marketCap: 1,
-            memooScore: 100,
-            price: 1,
-            ticker: '1',
-            tokenName: 'string',
-            volume24H: 1,
-            totalScore: 1,
-            created: '196d',
-            liquidity: '6.5m',
-            holders: '114k',
+        //     vol1h: '235m',
+        //   },
+        //   {
+        //     icon: 'string',
+        //     increase1H: 1,
+        //     increase24H: 1,
+        //     marketCap: 1,
+        //     memooScore: 100,
+        //     price: 1,
+        //     ticker: '1',
+        //     tokenName: 'string',
+        //     volume24H: 1,
+        //     totalScore: 1,
+        //     created: '196d',
+        //     liquidity: '6.5m',
+        //     holders: '114k',
 
-            vol1h: '235m',
-          },
-        ]);
+        //     vol1h: '235m',
+        //   },
+        // ]);
         setPagination({
           ...pagination,
           total: data.total_record ?? 0,
